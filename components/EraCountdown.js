b6ab07adf251b740363972993bac31eca4e9a46e
arnobase <arnaud.gardies@gmail.com>
2023-04-04 11:25:14 +0200

    update era countdown display

--- components/EraCountdown.js
+++ components/EraCountdown.js
@@ -5,18 +5,20 @@ import { EraEtaContext } from "../context/EraEtaProvider";
 const EraCountdown = (props) => {
 
   function EtaDisplay(props) {
+    if (eraeta !== undefined) {
     return <div className="flex items-center justify-center">
         <span>{props.eraeta}</span>
     </div>
+    }
   }
   const { eraeta } = useContext(EraEtaContext);
-  if (eraeta !== undefined) {
+  
     return <div className={`w-screen flex items-center justify-center mt-14`}>
       <div className="bg-[#191B1F] rounded-2xl px-8 py-8 ">
         <div className="flex items-center justify-center text-lg" >Next Era ETA:&nbsp;<EtaDisplay eraeta={eraeta}/></div>
         <span></span>
       </div>
     </div>;
-  }
+  
 };
 export default EraCountdown;
