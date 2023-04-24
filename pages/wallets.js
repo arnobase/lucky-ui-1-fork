e57d9e8b3587602c54693fd073fb7220ea7f0734
arnobase <arnaud.gardies@gmail.com>
2023-04-24 22:53:42 +0200

    include wallet list WIP

---
+++ pages/wallets.js
@@ -0,0 +1,13 @@
+import WalletInfos from "../components/WalletInfos";
+import Header from "../components/Header"
+import { ParticlesContainer } from "../components/ParticlesContainer";
+
+export default function Wallets() {
+  return (
+    <div>
+      <Header />
+      <WalletInfos/>
+  
+    </div>
+  );
+}
