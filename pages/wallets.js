2f08ae4c3940b196f89a30cdf74ddf3b2ca84706
arnobase <arnaud.gardies@gmail.com>
2023-04-26 14:46:42 +0200

    cleanup dependancies

--- pages/wallets.js
+++ pages/wallets.js
@@ -1,6 +1,5 @@
-import WalletInfos from "../components/walletInfos";
+import WalletInfos from "../components/WalletInfos";
 import Header from "../components/Header"
-import { ParticlesContainer } from "../components/ParticlesContainer";
 
 export default function Wallets() {
   return (
