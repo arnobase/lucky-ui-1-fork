6a599a3b32494ca6669937d1c0f1ceb86c3943a1
arnobase <arnaud.gardies@gmail.com>
2023-10-03 15:51:35 +0200

    X link in _blank

--- components/AccoutInfos.js
+++ components/AccoutInfos.js
@@ -84,7 +84,7 @@ const AccountInfos = () => {
             <div className="flex items-center justify-center">
             <div className="text-center">
               <div className="py-4">
-                <a className="tweetbutton" href="http://twitter.com/share?text=I won the @LuckyDapp  Raffle 💰🥳%0A%0AStake your $ASTR $SDN and be the lucky guy next time 🍀&url=https://lucky.substrate.fi&hashtags=AstarNetwork,LuckyDapp">
+                <a target="_blank" className="tweetbutton" href="http://twitter.com/share?text=I won the @LuckyDapp  Raffle 💰🥳%0A%0AStake your $ASTR $SDN and be the lucky guy next time 🍀&url=https://lucky.substrate.fi&hashtags=AstarNetwork,LuckyDapp">
                   <i></i> Share on X</a>
               </div>
               <div>📢 Invite other players 📢<br/>and make the Lucky raffle bigger next time</div>
