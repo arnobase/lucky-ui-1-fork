91a70bc3b771000d601ee59757553d61caddb218
arnobase <arnaud.gardies@gmail.com>
2024-02-10 18:31:54 +0100

    update header

--- components/Header.js
+++ components/Header.js
@@ -9,36 +9,35 @@ import { ApiContext } from "../context/ApiProvider.js";
 import { useContext } from "react";
 
 const headerStyle= {
-  title: ``,
-  //headwrapper: `p-4 w-screen flex justify-between items-center`,
-  headwrapper: ``,
-  buttonsContainer: ``,
-  buttonsContainerStart: `col-span-3 sm:col-span-2 flex justify-start items-center`,
-  
-  button: `max-h[50px] flex items-center content-block bg-[#191B1F] hover:bg-[#333437] rounded-2xl mx-2 text-[0.9rem] font-semibold cursor-pointer`,
-  buttonPadding: `p-2`,
-  buttonIconContainer: `flex items-center justify-center p-2`,
+  title: `col-span-3 sm:col-span-1 flex text-3xl text-white-700 text-center font-semibold  xs:min-w-full`,
+  headwrapper: `grid grid-cols-3 p-4 `,
+  buttonsContainer: `col-span-3 sm:col-span-2 flex justify-end items-center`,
 }
 
 function Header() {
   const { network } = useContext(ApiContext)
   const raffle_url = network === undefined ? "/astar" : "/"+network
   return (
-    <div className="grid grid-cols-8 p-4">
-      <div className="sm:col-span-8 md:col-span-2 gd:col-span-2 flex text-3xl text-white-700 text-center font-semibold  xs:min-w-full">
+    <div className={headerStyle.headwrapper}>
+      <div className={headerStyle.title}>
         <a href={raffle_url}><ExportedImage className="mr-1" src={LuckyLogo} alt="Lucky" height={75} /></a>
       </div>
+      {/*
       <div className="sm:col-span-8 md:col-span-3 gd:col-span-3 flex justify-end items-center"> 
         <a className="pr-8" href={raffle_url}>The Lucky Raffle</a>
         <a className="pr-8" href="/wallets">Wallet Overview</a> 
         <a className="pr-8" href="/wiki">Docs</a> 
       </div>
-      <div className="sm:col-span-8 md:col-span-3 gd:col-span-3 flex justify-end items-center"> 
+      */}
+      <div className={headerStyle.buttonsContainer}>
         <NetworkSelect/>    
         <AccountSelect />
       </div>
     </div>
   );
 }
+
+
+
 
 export default Header;
