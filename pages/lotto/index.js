b535e4bfb49320cd16e5662ef472a8e7e2a9bdfd
arnobase <arnaud.gardies@gmail.com>
2024-06-16 20:59:02 +0200

    redirect lotto to astar by default

---
+++ pages/lotto/index.js
@@ -0,0 +1,6 @@
+const isStaticExport = process.env.NEXT_PUBLIC_STATIC_EXPORT === 'true';
+export default function Home() {
+  if (typeof window !== "undefined") {
+    window.location.replace(isStaticExport ? "/lotto/astar.html" : "/lotto/astar")
+  }
+}
